63374e582324d769d360aaa279aed22d4ae65d23
flikas <flikas@outlook.com>
2020-10-07 13:49:12 +0800

    This chat room can be used to test if the server supports WebSocket.

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+    "transports": ["websocket"]
+});
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
